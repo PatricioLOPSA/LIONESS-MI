4ce3d1f6b0a497489bb5da654a22a44daf412dbc
PatricioLOPSA <patrick.losa90@gmail.com>
2024-03-01 11:23:33 -0600

    added script for filtering complete SSnets

---
+++ filter_networks.R
@@ -0,0 +1,24 @@
+library(data.table, quietly = F)
+library(dplyr, quietly = T)
+
+
+ sysargs <- commandArgs(trailingOnly = T)
+#
+ setwd(sysargs[1])
+
+files <- list.files(pattern="SSnet.tsv") %>% as.list()
+
+#aquí empieza la function:
+filter_nets <- function(x){
+net_id <- as.character(x)
+net <- fread(file = net_id) %>% as.data.frame()
+net <- net[order(- net[,3]),]
+nr_edges <- as.numeric(sysargs[2])
+#nr_edges <- 10000
+
+SS_name <- paste("filtered-",net_id, sep = "")
+
+fwrite(head(net,nr_edges), SS_name,sep = "\t")
+}
+
+lapply(files, filter_nets)
